4bfc143f2981c05431c7729f89351b7aa0127bfa
Saikat Chakraborty <saikatc@cs.columbia.edu>
2020-09-06 00:57:44 -0400

    Modify comment.

--- main.cpp
+++ main.cpp
@@ -106,7 +106,7 @@ std::vector<Token> tokenizeCode(std::string _character_stream){
      * You are free to change this function body as you deem necessary.
      * But the function prototype (i.e. function name, parameter and return type) must not be changed.
      * A partial implementation is given inside the function for your convenience.
-     * But, remember you don't have to follow this initial (partial) implementation procedure.
+     * But remember, you don't have to follow this initial (partial) implementation procedure.
      */
     std::string state_buffer; //Initially we start with an null or empty state.
     // We scan character by character.
